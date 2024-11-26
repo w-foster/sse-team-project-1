4c41ecceb81437332f4a99699db18b15129a9cf7
w-foster <william.jfoster@yahoo.com>
2024-11-26 17:23:37 +0000

    create supabase client & hardcode key/url for now

--- src/app.py
+++ src/app.py
@@ -1,12 +1,19 @@
 from flask import Flask, render_template
-
+from supabase_py import create_client
 
 app = Flask(__name__)
+
+# CHANGE THIS TO TSURU ENV VARS LATER!
+SUPABASE_URL = "https://lacstwcjmfdrnebmmpdl.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxhY3N0d2NqbWZkcm5lYm1tcGRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI1NTE1NzgsImV4cCI6MjA0ODEyNzU3OH0.tankWlViseqQUzaR5wxQfuJoc8WxTLl28jBOotlBPbY"
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    response = supabase.table('favourites').select('user_id').execute()
+    return str(response)
+    #return render_template('index.html')
 
 
 @app.route('/item')
