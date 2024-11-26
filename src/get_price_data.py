e8440a33da3b0d8376659eded4a97f349083d3d8
w-foster <william.jfoster@yahoo.com>
2024-11-26 19:19:48 +0000

    remove debugging print

--- src/get_price_data.py
+++ src/get_price_data.py
@@ -1,6 +1,7 @@
 import requests
 import json
 
+
 def get_price_data(item_id, timestep):
     api_url = f"https://prices.runescape.wiki/api/v1/osrs/timeseries?timestep={timestep}&id={item_id}"
 
@@ -13,8 +14,5 @@ def get_price_data(item_id, timestep):
 
         print(json.dumps(data, indent=4))
 
-
     except requests.exceptions.RequestException as e:
         print(f"Error: {e}")
-
-get_price_data(4151, "5m")
