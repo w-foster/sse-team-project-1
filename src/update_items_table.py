5569b39203ea93ccadb8d866a40ffbc2dd042b81
w-foster <william.jfoster@yahoo.com>
2024-11-26 18:26:30 +0000

    add script to get mapping data and upsert to table in supabase db

---
+++ src/update_items_table.py
@@ -0,0 +1,30 @@
+from supabase import create_client
+import requests
+import json
+
+# CHANGE THIS TO TSURU ENV VARS LATER!
+SUPABASE_URL = "https://lacstwcjmfdrnebmmpdl.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxhY3N0d2NqbWZkcm5lYm1tcGRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI1NTE1NzgsImV4cCI6MjA0ODEyNzU3OH0.tankWlViseqQUzaR5wxQfuJoc8WxTLl28jBOotlBPbY"
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+api_url = "https://prices.runescape.wiki/api/v1/osrs/mapping"
+
+try:
+    response = requests.get(api_url)
+
+    response.raise_for_status()
+
+    data = response.json()
+
+    for item in data:
+        if "limit" in item:
+            item["buylimit"] = item.pop("limit")
+
+
+    print(json.dumps(data, indent=4))
+
+    response = supabase.table('mapping_data').upsert(data).execute()
+
+except requests.exceptions.RequestException as e:
+    print(f"Error: {e}")
+
