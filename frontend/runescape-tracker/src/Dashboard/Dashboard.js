9258fd77dffd0750759a8b3191963301734b2961
w-foster <william.jfoster@yahoo.com>
2024-11-27 22:26:00 +0000

    re-add new fave props to Dashboard

--- frontend/runescape-tracker/src/Dashboard/Dashboard.js
+++ frontend/runescape-tracker/src/Dashboard/Dashboard.js
@@ -1,11 +1,14 @@
 import ItemGrid from './ItemGrid'
 import BasicLineChart from './Graph'
 
-export default function Dashboard({ className }) {
+export default function Dashboard({ className, favourites, addFavourite, removeFavourite }) {
     return (
         <div className={className}>
             Dashboard
-            <ItemGrid />
+            <ItemGrid
+                favourites={favourites}
+                addFavourite={addFavourite}
+                removeFavourite={removeFavourite} />
         </div>
     );
 }
