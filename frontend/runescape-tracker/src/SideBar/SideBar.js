a8a99c34c51da90fc1422356f8e857c42adfa731
w-foster <william.jfoster@yahoo.com>
2024-11-27 20:42:06 +0000

    new build for removing debugging faves list

--- frontend/runescape-tracker/src/SideBar/SideBar.js
+++ frontend/runescape-tracker/src/SideBar/SideBar.js
@@ -5,13 +5,10 @@ export default function SideBar({ className, favourites, removeFavourite }) {
     return (
         <div className={className}>
             <h1>SIDEBAR</h1>
-            {favourites.map((f) => (
-                <p>{f}</p>
-            ))}
             <FavouritesList 
                 favourites={favourites}
                 removeFavourite={removeFavourite} />
-            <SignInForm />
+            
         </div>
     );
 }
