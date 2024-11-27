48cbbcb209bbeda8d79ddbe82111b0f29f1740fd
Andy Peng <andy.x.p@outlook.com>
2024-11-27 14:47:58 +0000

    add dropdown list search

--- frontend/runescape-tracker/src/SearchBar/SearchBar.js
+++ frontend/runescape-tracker/src/SearchBar/SearchBar.js
@@ -1,8 +1,9 @@
-
+import AutocompleteIntroduction from './Search'
 
 export default function SearchBar({ className }) {
     return (
         <div className={className}>
+            <AutocompleteIntroduction />
             Search bar
         </div>
     );
