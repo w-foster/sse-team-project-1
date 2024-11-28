e235b5df35fd4b2e525bd4af6fe375db3b2ac154
Lucas Chan <150052194+lucas120301@users.noreply.github.com>
2024-11-28 14:36:39 +0000

    add hot item list on frontpage

--- frontend/runescape-tracker/src/components/Dashboard/Dashboard.js
+++ frontend/runescape-tracker/src/components/Dashboard/Dashboard.js
@@ -1,16 +1,25 @@
 import ItemGrid from './ItemGrid'
 import BasicLineChart from './Graph'
+import HotItemGrid from './HotItems';
 
 export default function Dashboard({ className, selectedItemID, itemName, favourites, addFavourite, removeFavourite }) {
     return (
         <div className={className}>
             
             {selectedItemID < 0 ? (
+                <>
                 <ItemGrid
                     favourites={favourites}
                     addFavourite={addFavourite}
                     removeFavourite={removeFavourite} 
                 />
+                {/* Hot Items List */}
+                <div style={{ marginTop: '20px' }}>
+                        <h2>Hot Items</h2>
+                        <HotItemGrid />
+                    </div>
+                </>
+
             ) : (
                 <BasicLineChart 
                     selectedItemID={selectedItemID} 
--- src/get_item_data.py
+++ src/get_item_data.py
@@ -4,14 +4,6 @@ import re
 import json
 from headers import headers # import headers
 
-file_path = 'frontend/runescape-tracker/src/components/common/SearchBar/ItemList.js'
-with open(file_path, 'r') as f:
-    content = f.read()
-    
-    # Extract the JSON-like part from the JavaScript file
-    match = re.search(r"=\s*(\[.*\]);", content, re.DOTALL)
-    json_content = match.group(1)  # Extract the array
-    item_list = json.loads(json_content)
 
 def get_api_data_items():
     api_url = 'https://prices.runescape.wiki/api/v1/osrs/latest'
@@ -27,13 +19,8 @@ def get_api_data_items():
         ]
 
         final_data = add_other_data(filtered_data)
+        add_name(final_data)
 
-        item_dict = {str(item['id']): item['name'] for item in item_list}
-
-    # Add the "name" field to API data based on the item ID
-        for item in final_data:
-            item_id = item.get('id')
-            item['name'] = item_dict.get(item_id, "Unknown Item")  # Default to "Unknown Item" if no match is found
         return final_data
     except requests.exceptions.RequestException as e:
         print(f"Error: {e}")
@@ -41,7 +28,6 @@ def get_api_data_items():
 
 def add_other_data(data):
     for item in data:
-        item["icon"] = f"https://services.runescape.com/m=itemdb_rs/obj_sprite.gif?id={item['id']}"
         item['favourite'] = False
         high = item.get("high")
         low = item.get("low")
@@ -51,6 +37,24 @@ def add_other_data(data):
         else:
                 # Handle cases where high or low is missing or invalid
                 item["margin_percentage"] = None
+    return data
+
+
+def add_name(data):
+    file_path = 'frontend/runescape-tracker/src/components/common/SearchBar/ItemList.js'
+    with open(file_path, 'r') as f:
+        content = f.read()
+    
+    # Extract the JSON-like part from the JavaScript file
+    match = re.search(r"=\s*(\[.*\]);", content, re.DOTALL)
+    json_content = match.group(1)  # Extract the array
+    item_list = json.loads(json_content)
+    item_dict = {str(item['id']): item['name'] for item in item_list}
+             # Add the "name" field to API data based on the item ID
+    for item in data:
+            item["icon"] = f"https://services.runescape.com/m=itemdb_rs/obj_sprite.gif?id={item['id']}"
+            item_id = item.get('id')
+            item['name'] = item_dict.get(item_id, "Unknown Item")  # Default to "Unknown Item" if no match is found
     return data
 
 if __name__ == "__main__":
