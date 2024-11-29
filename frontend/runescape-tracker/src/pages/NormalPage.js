34e06ec1b517a6d03925344addaf1e3b0997353c
w-foster <william.jfoster@yahoo.com>
2024-11-29 21:37:32 +0000

    remove GETs for a removed logo from create-react-app

--- frontend/runescape-tracker/src/pages/NormalPage.js
+++ frontend/runescape-tracker/src/pages/NormalPage.js
@@ -4,8 +4,8 @@ import { useSessionInfo } from '../SessionInfoContext';
 
 
 export default function NormalPage({ itemList, favourites, removeFavourite }) {
-    const currentUserId = useSessionInfo();
-    console.log("Current User:", JSON.stringify(currentUserId.user, null, 2));
+    const { userId } = useSessionInfo();
+    console.log("Current User:", JSON.stringify(userId, null, 2));
 
     return (
         <>
