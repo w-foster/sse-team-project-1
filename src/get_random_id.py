4cd9122fce59d5e26569c6db02cb7cbaf7c10f01
w-foster <william.jfoster@yahoo.com>
2024-12-02 22:43:58 +0000

    fix relative pathing of ItemList

--- src/get_random_id.py
+++ src/get_random_id.py
@@ -1,13 +1,25 @@
+import os
 import random
 import json
 
 def get_random_id():
-    with open('src/ItemList.json', 'r') as file:
-        item_list = json.load(file)
-    """
-    Get a random id from a list of items.
-    """
+    this_directory = os.path.dirname(os.path.abspath(__file__))
 
+    # Possible locations of ItemList.json
+    possible_paths = [
+        os.path.join(this_directory, 'ItemList.json'),
+        os.path.join(this_directory, 'src', 'ItemList.json')
+    ]
+
+    # Try to find the file in the possible paths
+    for path in possible_paths:
+        if os.path.isfile(path):
+            with open(path, 'r') as file:
+                item_list = json.load(file)
+            break
+    else:
+        raise FileNotFoundError("Could not find 'ItemList.json' in expected locations.")
+    
     # Generate a random element from the list
     random_item = random.choice(item_list)
 
