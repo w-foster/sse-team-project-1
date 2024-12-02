e60afefc27717d1119928efcd7d8b59152d4e5e6
Andy Peng <andy.x.p@outlook.com>
2024-12-02 16:00:58 +0000

    add catchall

--- src/app.py
+++ src/app.py
@@ -150,5 +150,10 @@ def popular_items():
     return jsonify(response)
 
 
+@app.route('/<path:path>')
+def catchall(path):
+    return send_from_directory(app.static_folder, "index.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
