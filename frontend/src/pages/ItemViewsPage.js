50abf52f8c33579b6780571a4aa6e59e021c5844
Lucas Chan <150052194+lucas120301@users.noreply.github.com>
2024-12-02 19:00:07 +0000

    fix most viewed item graph y axis

--- frontend/src/pages/ItemViewsPage.js
+++ frontend/src/pages/ItemViewsPage.js
@@ -2,14 +2,7 @@ import ItemViews from "../components/ItemViewsComponents/ItemViews.js";
 
 export default function ItemViewsPage({ idToNameMap }) {
   return (
-    <div
-      className="debug-main-content"
-      style={{
-        outline: "2px solid red", // Add an outline to visually debug the container
-        margin: "10px", // Add margin for clarity
-        padding: "10px", // Ensure padding inside the container
-      }}
-    >
+<div>
       <ItemViews idToNameMap={idToNameMap} />
     </div>
   );
