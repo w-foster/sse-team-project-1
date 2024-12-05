6ae19764eeddee5ecc7a9ac6a17ef1366c6b9bb2
Andy Peng <andy.x.p@outlook.com>
2024-12-05 11:43:25 +0000

    extend border of main content to all pages

--- frontend/src/utils/tailwindClasses.js
+++ frontend/src/utils/tailwindClasses.js
@@ -1,13 +1,13 @@
 // src/utils/tailwindClasses.js
 
 export const mainAllItemsContainer = `
-  fixed top-[8vh] w-[100vw] h-[calc(100vh-8vh)] p-5 box-border overflow-y-auto z-0
-  border-[3px] border-cyan-800 bg-primaryLightBackground dark:bg-primaryDarkBackground
-  text-primaryLight dark:text-primaryDark
+  fixed top-[8vh] w-[100vw] h-[calc(100vh-8vh)] p-5 box-border overflow-y-auto z-0 border-t border-zinc-700
+    bg-primaryLightBackground dark:bg-primaryDarkBackground
+    text-primaryLight dark:text-primaryDark
 `;
 
 export const mainAlchemyContainer = `
-    fixed top-[8vh] left-[20vw] w-[calc(100vw-20vw)] h-[calc(100vh-8vh)] border-3 border-solid border-green-500 p-5 box-border overflow-y-auto z-0
+    fixed top-[8vh] left-[20vw] w-[calc(100vw-20vw)] h-[calc(100vh-8vh)] p-5 box-border overflow-y-auto z-0 border-t border-zinc-700
     bg-primaryLightBackground dark:bg-primaryDarkBackground
     text-primaryLight dark:text-primaryDark
 `;
@@ -19,13 +19,13 @@ export const about = `
 `;
 
 export const mainGraphContainer = `
-    fixed top-[8vh] left-[20vw] w-[calc(100vw-20vw)] h-[calc(100vh-8vh)] border-3 border-solid border-green-500 p-5 box-border overflow-y-auto z-0
+    fixed top-[8vh] left-[20vw] w-[calc(100vw-20vw)] h-[calc(100vh-8vh)] p-5 box-border overflow-y-auto z-0 border-t border-zinc-700
     bg-primaryLightBackground dark:bg-primaryDarkBackground
     text-primaryLight dark:text-primaryDark
 `;
 
 export const mainItemViews = `
-    fixed top-[8vh] left-[20vw] w-[calc(100vw-20vw)] h-[calc(100vh-8vh)] border-3 border-solid border-green-500 p-5 box-border overflow-y-auto z-0
+    fixed top-[8vh] left-[20vw] w-[calc(100vw-20vw)] h-[calc(100vh-8vh)] p-5 box-border overflow-y-auto z-0 border-t border-zinc-700
     bg-primaryLightBackground dark:bg-primaryDarkBackground
     text-primaryLight dark:text-primaryDark
 `;
