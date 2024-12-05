fd87eee827c2e3939b6cddb5eb816930698e030d
Andy Peng <andy.x.p@outlook.com>
2024-12-05 18:26:28 +0000

    update dark mode for 404

--- frontend/src/pages/404.js
+++ frontend/src/pages/404.js
@@ -1,13 +1,13 @@
 export default function Example() {
 	return (
 	  <>
-		<main className="grid h-screen place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+		<main className="grid h-screen place-items-center bg-white dark:bg-primaryDarkBackground px-6 py-24 sm:py-32 lg:px-8">
 		  <div className="text-center">
 			<p className="text-base font-semibold text-indigo-600">404</p>
-			<h1 className="mt-4 text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">
+			<h1 className="mt-4 text-balance text-5xl font-semibold tracking-tight text-gray-900 dark:text-primaryDark sm:text-7xl">
 			  Page not found
 			</h1>
-			<p className="mt-6 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">
+			<p className="mt-6 text-pretty text-lg font-medium text-gray-500 dark:text-gray-300 sm:text-xl/8">
 			  Sorry, we couldn’t find the page you’re looking for.
 			</p>
 			<div className="mt-10 flex items-center justify-center gap-x-6">
