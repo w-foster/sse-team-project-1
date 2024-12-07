b33496c577ee76f8ca942d73ff5f96e11c41c71a
Andy Peng <andy.x.p@outlook.com>
2024-12-07 17:56:35 +0000

    edit test.py

--- src/test.py
+++ src/test.py
@@ -1,2 +1,9 @@
-import pytest
-from get_price_data import get_price_data
+from get_price_data import get_graph_data
+
+
+def test_get_price_data():
+    assert get_graph_data(1231312, "5m") is None  # Use 'is' to check for None
+
+
+if __name__ == "__main__":
+    test_get_price_data()
