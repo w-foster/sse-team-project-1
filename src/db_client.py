23e3779a00b19e38c7cca463a0c1c5105dbdb447
Yile Huang <yilehuang2@gmail.com>
2024-12-09 19:00:22 +0000

    adjust python formatting

--- src/db_client.py
+++ src/db_client.py
@@ -2,15 +2,6 @@ import os
 from dotenv import load_dotenv
 from supabase import create_client
 
-# CHANGE THIS TO TSURU ENV VARS LATER!
-# SUPABASE_URL = "https://lacstwcjmfdrnebmmpdl.supabase.co"
-# SUPABASE_KEY = (
-#     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ey"
-#     "Jpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxhY3N0d"
-#     "2NqbWZkcm5lYm1tcGRsIiwicm9sZSI6ImFub24i"
-#     "LCJpYXQiOjE3MzI1NTE1NzgsImV4cCI6MjA0ODE"
-#     "yNzU3OH0.tankWlViseqQUzaR5wxQfuJoc8WxTLl28jBOotlBPbY"
-# )
 
 # Load environment variables from .env file (for local development)
 if os.getenv("TSURU_APPNAME") is None:  # Check if running in Tsuru
